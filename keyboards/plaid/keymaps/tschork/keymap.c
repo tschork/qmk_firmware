8aa92293811ec498b5cd7ba6297801fe3a84c5e7
Tripy <thierry@schork.ch>
2020-05-08 00:31:54 +0200

    * enabling numlock at keyboard boot so that numpad works * moved mouse keys 1 column to the right * commented layers, now to give a visual representation like the default keymap

--- keyboards/plaid/keymaps/tschork/keymap.c
+++ keyboards/plaid/keymaps/tschork/keymap.c
@@ -1,14 +1,57 @@
 #include QMK_KEYBOARD_H
+
 /*
 Swiss french layout, derived from a TKL keyboard.
-
 Feel free to get inspiration from this, I take no responsabilities for hair loss if debugging this thing.
 
+todo:  make the MO sitches to TG and use the leds as layer indicator.
+        use the 2 leds like binary clocks (o is off, x is on)
+            o o layer 0
+            x o layer 1
+            o x layer 2
+            x x layer 3
+
+7.05.2020   enabling numlock at keyboard boot
+            moved mouse keys 1 column to the right
+            commented layers, now to give a visual representation like the default keymap
 */
 
+//force numlock to on on keyboard boot
+void matrix_init_user(void) {
+    if (!(host_keyboard_leds() & (1 << USB_LED_NUM_LOCK))) {
+        register_code(KC_NUMLOCK);
+        unregister_code(KC_NUMLOCK);
+    }
+}
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_plaid_mit(KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Z, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_LBRC, KC_RBRC, KC_LSFT, KC_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_SCLN, KC_QUOT, KC_BSLS, KC_SFTENT, KC_LCTL, KC_LALT, KC_RALT, KC_LGUI, MO(1), LT(3,KC_SPC), MO(2), KC_COMM, KC_DOT, KC_SLSH, KC_NUBS),
-	[1] = LAYOUT_plaid_mit(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO, KC_HOME, KC_PGUP, KC_DEL, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_MPRV, KC_END, KC_PGDN, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_SPC, KC_NO, KC_MFFD, KC_VOLD, KC_VOLU, KC_MPLY),
-	[2] = LAYOUT_plaid_mit(KC_ESC, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_MS_U, KC_BTN2, KC_MINS, KC_EQL, KC_BSPC, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_NO, KC_DEL, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN3, KC_MPRV, KC_NO, KC_NO, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_SPC, KC_NO, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
-	[3] = LAYOUT_plaid_mit(DEBUG, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PAST, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_PPLS)
+	//base layer, qwertz
+    [0] = LAYOUT_plaid_mit(
+		KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_LBRC, KC_RBRC,
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_SCLN, KC_QUOT, KC_BSLS, KC_SFTENT,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_RALT, MO(1), LT(3,KC_SPC), MO(2), KC_COMM, KC_DOT, KC_SLSH, KC_NUBS
+	),
+    //F keys and numbers
+	[1] = LAYOUT_plaid_mit(
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+		KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO, KC_HOME, KC_PGUP, KC_DEL,
+		KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_MPRV, KC_END, KC_PGDN, KC_ENT,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_SPC, KC_NO, KC_MFFD, KC_VOLD, KC_VOLU, KC_MPLY
+	),
+    //mouse emulation and wasd as arrows
+	[2] = LAYOUT_plaid_mit(
+		KC_ESC, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_MS_U, KC_MINS, KC_EQL, KC_BSPC,
+		KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN3, KC_DEL,
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_MPRV, KC_BTN2, KC_NO, KC_ENT,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_SPC, KC_NO, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+	),
+    //wasd as arrows and numeric keypad
+	[3] = LAYOUT_plaid_mit(
+		DEBUG, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_PSLS,
+		KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PAST,
+		KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PMNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_PENT, KC_PPLS
+	)
 };
--- quantum/version.h
+++ quantum/version.h
@@ -1,4 +1,4 @@
-#define QMK_VERSION "0.8.147-8-g123945-dirty"
-#define QMK_BUILDDATE "2020-05-07-01:24:44"
+#define QMK_VERSION "0.8.147-9-g721de8-dirty"
+#define QMK_BUILDDATE "2020-05-08-00:18:28"
 #define CHIBIOS_VERSION "breaking_2020_q1"
 #define CHIBIOS_CONTRIB_VERSION "breaking_2020_q1"
