d1c9379db63dcfd14b4e32cc04393cae010545d6
Tripy <thierry@schork.ch>
2020-05-11 23:12:52 +0200

    * tap dancing on enter => tab

--- quantum/version.h
+++ quantum/version.h
@@ -1,4 +1,4 @@
-#define QMK_VERSION "0.8.147-32-g71f6e1-dirty"
-#define QMK_BUILDDATE "2020-05-11-22:49:39"
+#define QMK_VERSION "0.8.147-33-gcddd94-dirty"
+#define QMK_BUILDDATE "2020-05-11-23:11:23"
 #define CHIBIOS_VERSION "breaking_2020_q1"
 #define CHIBIOS_CONTRIB_VERSION "breaking_2020_q1"
