7f8ee7376521a643ceb6ddb9208fd8f19c984035
Tripy <thierry@schork.ch>
2020-05-13 21:35:13 +0200

    updated the layout to propose my most used but hard to remember keys from the plaid keyboard

--- keyboards/keycapsss/plaid_pad/keymaps/tschork/keymap.c
+++ keyboards/keycapsss/plaid_pad/keymaps/tschork/keymap.c
@@ -4,20 +4,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /*
  * ,-----------------------,
- * |  7  |  8  |  9  |  F5 |
+ * |  '  |  =  |  !  |  F5 |
  * |-----+-----+-----+-----|
- * |  4  |  5  |  6  |  F12|
+ * |  :  |  /  |  \  |  F12|
  * |-----+-----+-----+-----|
- * |  1  |  2  |  3  |  up |
+ * |  @  | del |  up |Enter|
  * |-----+-----+-----+-----|
- * |  0  | Left| rght|  Dwn|
+ * |  #  | Left| Dwn|  rght|
  * `-----------------------'
  */
 LAYOUT_ortho_4x4(
-		KC_P7, KC_P8,   KC_P9,      KC_F5,
-		KC_P4, KC_P5,   KC_P6,      KC_F12,
-		KC_P1, KC_P2,   KC_P3,      KC_UP,
-		KC_P0, KC_LEFT, KC_RGHT,    KC_DOWN
+		KC_MINS, LSFT(KC_0), LSFT(KC_RBRC), KC_F5,
+		LSFT(KC_DOT), LSFT(KC_7), RALT(KC_NUBS), KC_F12,
+		RALT(KC_2), KC_DEL,   KC_UP,  KC_ENTER,
+		RALT(KC_3), KC_LEFT, KC_DOWN, KC_RGHT
 )
 
 };
--- quantum/version.h
+++ quantum/version.h
@@ -1,4 +1,4 @@
-#define QMK_VERSION "0.8.147-33-gcddd94-dirty"
-#define QMK_BUILDDATE "2020-05-11-23:11:23"
+#define QMK_VERSION "0.8.147-36-gb57f09-dirty"
+#define QMK_BUILDDATE "2020-05-13-19:46:38"
 #define CHIBIOS_VERSION "breaking_2020_q1"
 #define CHIBIOS_CONTRIB_VERSION "breaking_2020_q1"
