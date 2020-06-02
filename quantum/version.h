0845d04bffca7939ee74de9cb81a08df5884299f
Tripy <thierry@schork.ch>
2020-06-02 06:35:03 +0200

    * added arrows cluster next to the enter key

--- quantum/version.h
+++ quantum/version.h
@@ -1,4 +1,4 @@
-#define QMK_VERSION "0.8.147-37-g7f8ee7-dirty"
-#define QMK_BUILDDATE "2020-05-19-20:07:26"
+#define QMK_VERSION "0.8.147-38-gbe940f-dirty"
+#define QMK_BUILDDATE "2020-05-20-16:29:00"
 #define CHIBIOS_VERSION "breaking_2020_q1"
 #define CHIBIOS_CONTRIB_VERSION "breaking_2020_q1"
